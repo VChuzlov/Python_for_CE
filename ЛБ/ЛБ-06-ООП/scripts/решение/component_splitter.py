49f9f49aff780678a90ee2207450aa5e48cfd99a
VChuzlov <58879294+VChuzlov@users.noreply.github.com>
2023-12-19 23:05:44 +0700

    LB-06

--- ЛБ/ЛБ-06-ООП/scripts/решение/component_splitter.py
+++ ЛБ/ЛБ-06-ООП/scripts/решение/component_splitter.py
@@ -9,7 +9,7 @@ class ComponentSplitter:
             split_coeffs: np.ndarray
     ) -> flow.Flow:
         new_molar_fractions = f.molar_fractions * split_coeffs
-        new_flow = 
+        new_flow = ...
         return ...
     
 
